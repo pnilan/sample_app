48ea20feab5109815846af9e7e36f2899781bcff
pnilan <nilan.patrick@gmail.com>
2014-01-12 15:49:02 -0800

    Make a basic User modle (including secure passwords)

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140107071949) do
+ActiveRecord::Schema.define(version: 20140112221615) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
+    t.string   "passowrd_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
