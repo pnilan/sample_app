34c5d6bcb66d7d434956474d7bc45e441755137a
pnilan <nilan.patrick@gmail.com>
2014-03-15 12:45:56 -0700

    start chapter 11

--- app/assets/javascripts/microposts.js
+++ app/assets/javascripts/microposts.js
@@ -1,15 +1,22 @@
+// Character Countdown
+
 function updateCountdown() {
 	// 140 is the maximum message length
-	var remaining = 140 - $('#micropost_content').val().length;
+	var max = 140
+	var remaining = max - $('#micropost_content').val().length;
 
 	if(remaining === 1) {
-		$('.countdown').text(remaining + ' character remaining.');
+		$('.countdown').text('1 character remaining.');
+		// countdownAttributes(s);
 	} else if(remaining === -1) {
-		$('.countdown').text(remaining*(-1) + ' character over.');
-	} else if(remaining < 0 && remaining != -1) {
+		$('.countdown').text('1 character over.');
+		// countdownAttributes();
+	} else if(remaining < -1) {
 		$('.countdown').text(remaining*(-1) + ' characters over.');
+		// countdownAttributes();
 	} else {
 		$('.countdown').text(remaining + ' characters remaining.');
+		// countdownAttributes();
 	};
 }
 
@@ -20,3 +27,19 @@ $(document).ready(function($) {
 	$('#micropost_content').keydown(updateCountdown);
 	$('#micropost_content').keypress(updateCountdown);
 });
+
+function countdownAttributes() {
+	var max = 140
+	var remaining = max - $('#micropost_content').val().length;
+
+	if(remaining <= 20 && remaining > 0) {
+		// font color = yellow
+		// $('.countdown').css('color', 'yellow');
+	} else if(remaining <= 0) {
+		// font color = red
+		// $('.countdown').css('color', 'red');
+	} else {
+		// font color = $grayLight
+		// $('.countdown').css('color', '$grayLight');
+	};
+}
